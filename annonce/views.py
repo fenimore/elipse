da23250aebe903f6bdefb9d527ab8f9d521097e9
polypmer <exorable.ludos@gmail.com>
2015-11-13 20:12:00 +0100

    models and forms

--- annonce/views.py
+++ annonce/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Donne, Pret, Exchice
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from .forms import DonneForm
+from django.utils import timezone
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+
+
+def index(request):
+    return render(request, 'annonce/index.html')
+
+##########################
+# Gift Views
+##########################
+# Post Gift
+# View Gifts
+# View Gift
